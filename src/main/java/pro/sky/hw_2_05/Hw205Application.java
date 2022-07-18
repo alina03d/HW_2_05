97df2441a25c99eb6a92d85899ddb45df19bdb34
alina <alina.daduts@gmail.com>
2022-07-18 22:43:51 +0300

    1 commit

---
+++ src/main/java/pro/sky/hw_2_05/Hw205Application.java
@@ -0,0 +1,13 @@
+package pro.sky.hw_2_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw205Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw205Application.class, args);
+    }
+
+}
